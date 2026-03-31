60bb4249d720113423052c22453bc5eab53e31c8
Luke Shull <lukeshull@tamu.edu>
2026-03-30 21:20:42 -0500

    feat: add boba.png

--- src/main/webui/src/routes/customer/+page.svelte
+++ src/main/webui/src/routes/customer/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
     import { resolve } from '$app/paths';
+    import { onMount } from 'svelte';
 
     function proceed() {
         goto(resolve('/customer/order'));
     }
+
+    
 </script>
+
+
 
 <div
     class="splash"
@@ -13,27 +18,55 @@
     on:keydown={proceed}
     tabindex="0"
 >
+    <div class="left">
+        <img src="/boba.png" alt="boba" />
+    </div>
 
     <div class="top-center"
     >Welcome to our Boba Shop!</div>
     <div class="center"
     
-    >Click anywhere to begin your order</div>
+    >Tap anywhere to begin your order</div>
+    <div class="weather">College Station, TX</div>
 </div>
 
 
 
 <style>
 
+    .left {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        left: 10%;
+    }
+
+    .left img {
+        max-width: 80%;
+        max-height: 80%;
+        object-fit: contain;
+    }
+
+    .weather {
+        position: absolute;
+        top: 95%;
+        left: 50%;
+        font-size: 1.2rem;
+        text-align: center;
+    }
+
     .top-center {
         position: absolute;
         top: 20px;
         left: 50%;
         transform: translateX(-50%);
+        text-align: center;
     }
 
     .center {
         position: absolute;
+        text-align: center;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
